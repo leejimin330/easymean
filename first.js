f6e0e015b589164ff9c406230826c43f5e2f8e1c
leejimin330 <54019102+leejimin330@users.noreply.github.com>
2020-09-23 10:45:54 +0900

    Add files via upload

---
+++ first.js
@@ -0,0 +1,11 @@
+/* jslint devel: true */
+/* eslint-disable no-console */
+/* eslint no-undef: "error" */
+/* eslint-env node */
+/* eslint-env browser */
+function viewCurrDate() { // eslint-disable-line no-unused-vars
+    "use strict";
+    document.getElementById('demo').innerHTML = Date();
+}
+
+viewCurrDate();
